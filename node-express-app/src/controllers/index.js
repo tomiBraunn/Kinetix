ab499acc3553cca8da78ea4745e0962e536260ba
Tomas B <tomasbraun2008@gmail.com>
2026-05-16 19:11:32 -0300

    hacer controllers

--- node-express-app/src/controllers/index.js
+++ node-express-app/src/controllers/index.js
@@ -1,8 +1,8 @@
 export const authController = {
     register: (req, res) => {
-        // Registration logic will go here
+        
     },
     login: (req, res) => {
-        // Login logic will go here
+        
     }
 };
